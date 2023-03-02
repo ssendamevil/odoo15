4f87de6009b709d2662390560d4595ee26bf9ead
Ali <ssendamevil@gmail.com>
2023-03-02 21:47:12 +0600

    first commit

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'summary': 'Hospital management system',
+    'description': """Hospital management system""",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+    'application': True,
+    'post_init_hook': '_synchronize_cron',
+    'license': 'LGPL-3',
+}
